2d22f3b8b9f8af9fc846c372a588e6582a450f7d
yonglinnnnnn <yongglinx@gmail.com>
2022-06-14 11:58:01 +0800

    push not working image upload

---
+++ Edutor/public/js/common.js
@@ -0,0 +1,5 @@
+// Display selected file name
+$(".custom-file-input").on("change", function () {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+})
