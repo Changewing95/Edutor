c87c97dfe68c9c57054c34d49ea7ca4731105960
changewing95 <changewing95@gmail.com>
2022-06-14 13:18:00 +0800

    test

--- Edutor/app.js
+++ Edutor/app.js
@@ -101,7 +101,7 @@ const dashboardRoute = require('./routes/dashboard');
 app.use('/', mainRoute);
 app.use('/auth', authRoute);
 app.use('/tutor/consultation', bookingRoute);
-app.use('/dashboard', dashboardRoute);
+// app.use('/dashboard', dashboardRoute);
 
 
 
