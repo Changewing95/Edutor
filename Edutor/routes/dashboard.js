82e365da9ad0b9b6ffd2f02ba3b7835dd2596501
changewing95 <changewing95@gmail.com>
2022-06-14 19:03:58 +0800

    Done with C R U D for student

--- Edutor/routes/dashboard.js
+++ Edutor/routes/dashboard.js
@@ -2,28 +2,30 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const flashMessage = require('../helpers/messenger');
-
+const UserController = require('../Controller/User');
 router.get('/overview', (req, res) => {
     res.render('dashboard/overview', {layout: 'main2', currentpage: {overview: true}});
 });
 
 
-router.get('/settings', (req, res) => {
-    res.render('dashboard/settings', {layout: 'main2', currentpage: {settings: true}});
-});
+// CRUD FEATURE FOR USER EDITING
 
+
+// READ
+router.get('/settings', async (req, res) => {
+    let user = await User.findOne({ where: { id: req.user.id } });
+    res.render('dashboard/settings', {layout: 'main2', currentpage: {settings: true}, name:user.name, email: user.email});
+});
 
 
 //  DELETE
-router.get('/settings/delete_student', async (req, res) => {
-    User.destroy({
-        where: {"id" : req.user.id}
-    }).then(() => {
-        flashMessage(res, 'success', "Account deleted!")
-        req.logout();
-        res.redirect('/');
-    })
-});
+router.get('/settings/delete_student', UserController.DeleteUser);
+
+
+
+// UPDATE
+router.post('/settings', UserController.CheckIfUserExists, UserController.UpdateUser);
+
 
 
 module.exports = router;
