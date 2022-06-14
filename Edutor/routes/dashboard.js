2adccf03354ea8581a828e8620efccbf07314422
changewing95 <changewing95@gmail.com>
2022-06-14 16:27:51 +0800

    Finish Delete/Dashboard

--- Edutor/routes/dashboard.js
+++ Edutor/routes/dashboard.js
@@ -4,15 +4,26 @@ const User = require('../models/User')
 const flashMessage = require('../helpers/messenger');
 
 router.get('/overview', (req, res) => {
-    res.render('dashboard/overview', {layout: 'main2'});
+    res.render('dashboard/overview', {layout: 'main2', currentpage: {overview: true}});
 });
 
 
-// Testing 123
+router.get('/settings', (req, res) => {
+    res.render('dashboard/settings', {layout: 'main2', currentpage: {settings: true}});
+});
 
 
 
-
+//  DELETE
+router.get('/settings/delete_student', async (req, res) => {
+    User.destroy({
+        where: {"id" : req.user.id}
+    }).then(() => {
+        flashMessage(res, 'success', "Account deleted!")
+        req.logout();
+        res.redirect('/');
+    })
+});
 
 
 module.exports = router;
