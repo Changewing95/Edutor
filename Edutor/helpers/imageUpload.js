5be3a94006b7ed25aa4a6c3439a13c6aaa6ff698
yonglinnnnnn <yongglinx@gmail.com>
2022-06-20 15:51:06 +0800

    multer upload fixed

--- Edutor/helpers/imageUpload.js
+++ Edutor/helpers/imageUpload.js
@@ -1,40 +1,39 @@
 const multer = require('multer');
 const path = require('path');
+
 // Set Storage Engine
 const storage = multer.diskStorage({
-    /* change 1 to req.user.id */
-    destination: (req, file, cb) => {
-        cb(null, './public/uploads/' + 1 + '/');
+    destination: (req, file, callback) => {
+        callback(null, './public/uploads/' + 1 + '/');
     },
-    /* change 1 to req.user.id */
-    filename: (req, file, cb) => {
-        cb(null, 1 + '-' + Date.now() +
-            path.extname(file.originalname));
+    filename: (req, file, callback) => {
+        callback(null, 1 + '-' + Date.now() + path.extname(file.originalname));
     }
 });
-// Check File Type
-function checkFileType(file, cb) {
-    // Allowed file extensions
+
+// Check File Type 
+function checkFileType(file, callback) {
+    // Allowed file extensions 
     const filetypes = /jpeg|jpg|png|gif/;
-    // Test extension
-    const extname =
-        filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Test mime
+    // Test extension 
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Test mime 
     const mimetype = filetypes.test(file.mimetype);
     if (mimetype && extname) {
-        return cb(null, true);
+        return callback(null, true);
     }
     else {
-        cb({ message: 'Images Only' });
+        callback({ message: 'Images Only' });
     }
 }
+
 // Define Upload Function
 const upload = multer({
     storage: storage,
     limits: { fileSize: 1000000 }, // 1MB
-    fileFilter: (req, file, cb) => {
-        checkFileType(file, cb);
+    fileFilter: (req, file, callback) => {
+        checkFileType(file, callback);
     }
-}).single('consultationUpload'); // Must be the name as the HTML file upload input
+}).single('consultationUpload'); // Must be the name as the HTML file upload input 
 
 module.exports = upload;
--- Edutor/routes/tutorConsultation.js
+++ Edutor/routes/tutorConsultation.js
@@ -1,3 +1,90 @@
+// const moment = require('moment');
+// const express = require('express');
+// const router = express.Router();
+// const Consultation = require('../models/Booking');
+// const flashMessage = require('../helpers/messenger');
+// const fs = require('fs');
+// const upload = require('../helpers/imageUpload');
+// const { start } = require('repl');
+
+// // const upload = require('../helpers/imageUpload');
+// // const ensureAuthenticated = require('../helpers/auth');
+
+// // ROUTING:
+// // route to catalogue for consultation
+// router.get('/main', (req, res) => {
+//     Consultation.findAll({
+//         // where: { userId: req.user.id },
+//         order: [['date']],
+//         raw: true
+//     })
+//         .then((consultations) => {
+//             // pass object to consultation.hbs
+//             res.render('consultation/consultation', { consultations });
+//         })
+//         .catch(err => console.log(err));
+//     // res.render('tutor/consultation');
+// });
+
+// // route to form field -- add slot
+// router.get('/create', (req, res) => {
+//     res.render('consultation/addConsultation');
+// });
+
+
+
+// // CODING LOGIC (CRUD)
+// // CREATE
+// router.post('/create', async function (req, res) {
+//     let title = req.body.title;
+//     let consultationURL = req.body.consultationURL;
+//     let price = req.body.price;
+//     let description = req.body.description;
+//     let start_time = req.body.start_time;
+//     let end_time = req.body.end_time;
+//     let date = moment(req.body.consultDate, 'DD/MM/YYYY');
+
+//     const message = 'Consultation slot successfully submitted';
+//     flashMessage(res, 'success', message);
+
+//     let consultation = await Consultation.create({ title, consultationURL, price, description, date, start_time, end_time })
+//     // .then((consultation) => {
+//     //     console.log(video.toJSON());
+//     //     res.redirect('/video/listVideos');
+//     // })
+//     // .catch(err => console.log(err));
+
+
+//     res.redirect('/tutor/consultation/main');
+// });
+
+// router.post('/upload', (req, res) => {
+//     // create user id directory for upload if not exist
+//     /* change 1 to req.user.id */
+//     if (!fs.existsSync('./public/uploads/' + 1)) {
+//         fs.mkdirSync('./public/uploads/' + 1, {
+//             recursive:
+//                 true
+//         });
+//     }
+//     upload(req, res, (err) => {
+//         if (err) {
+//             // e.g. File too large
+//             res.json({ file: '/img/no-image.jpg', err: err });
+//         }
+//         else {
+//             /* change 1 to req.user.id */
+//             res.json({
+//                 file: `/uploads/1/${req.file.filename}`
+//             });
+//         }
+//     });
+// });
+
+
+// module.exports = router;
+
+
 const moment = require('moment');
 const express = require('express');
 const router = express.Router();
@@ -5,9 +92,7 @@ const Consultation = require('../models/Booking');
 const flashMessage = require('../helpers/messenger');
 const fs = require('fs');
 const upload = require('../helpers/imageUpload');
-const { start } = require('repl');
 
-// const upload = require('../helpers/imageUpload');
 // const ensureAuthenticated = require('../helpers/auth');
 
 // ROUTING: 
@@ -48,11 +133,11 @@ router.post('/create', async function (req, res) {
     flashMessage(res, 'success', message);
 
     let consultation = await Consultation.create({ title, consultationURL, price, description, date, start_time, end_time })
-    // .then((consultation) => {
-    //     console.log(video.toJSON());
-    //     res.redirect('/video/listVideos');
-    // })
-    // .catch(err => console.log(err));
+        .then((consultation) => {
+            console.log(video.toJSON());
+            res.redirect('/video/listVideos');
+        })
+        .catch(err => console.log(err));
 
 
     res.redirect('/tutor/consultation/main');
@@ -60,7 +145,6 @@ router.post('/create', async function (req, res) {
 
 router.post('/upload', (req, res) => {
     // create user id directory for upload if not exist
-    /* change 1 to req.user.id */
     if (!fs.existsSync('./public/uploads/' + 1)) {
         fs.mkdirSync('./public/uploads/' + 1, {
             recursive:
@@ -70,15 +154,15 @@ router.post('/upload', (req, res) => {
     upload(req, res, (err) => {
         if (err) {
             // e.g. File too large
-            res.json({ file: '/img/no-image.jpg', err: err });
+            res.json({ file: '/uploads/profile/profile.png', err: err });
         }
         else {
-            /* change 1 to req.user.id */
             res.json({
                 file: `/uploads/1/${req.file.filename}`
             });
         }
     });
+
 });
 
 
