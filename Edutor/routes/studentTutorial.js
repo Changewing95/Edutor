8d768c2263988eb4013fb12c26093f093326d7cf
x3BoffiNg <85842044+x3BoffiNg@users.noreply.github.com>
2022-07-19 09:51:53 +0800

    Video up for student and admin side

--- Edutor/routes/studentTutorial.js
+++ Edutor/routes/studentTutorial.js
@@ -28,10 +28,17 @@ router.get('/display/:id', (req, res) => {
         .catch(err => console.log(err));
 });
 
-router.get('/get-video/:fileName', (req, res) => {
+router.get('/get-video/:userId/:fileName', async (req, res) => {
     // res.sendFile(`uploads/${id}/${req.params.fileName}`, { root: 'public' })
-
-    res.sendFile(`uploads/${req.user.id}/${req.params.fileName}`, { root: 'public' })
+    // await Tutorial.findByPk(req.params.fileName)
+    //     let tut = await Tutorial.findByPk
+    //     // .then((tutorials) => {
+    //         console.log(tut)
+    //         console.log(req.params.fileName)
+            // res.sendFile(`uploads/${tutorials.userId}/${req.params.fileName}`, { root: 'public' })
+        // .catch(err => console.log(err));
+    console.log(req.params.userId);
+    res.sendFile(`uploads/${req.params.userId}/${req.params.fileName}`, { root: 'public' })
 })
 
 module.exports = router;
