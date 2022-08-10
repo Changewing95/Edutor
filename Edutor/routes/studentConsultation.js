a8235d75ef764c7c05b7503223a24c3626faaee8
yonglinnnnnn <yongglinx@gmail.com>
2022-08-10 17:23:05 +0800

    enable display of PURCHASED consultation detail for students

--- Edutor/routes/studentConsultation.js
+++ Edutor/routes/studentConsultation.js
@@ -2,9 +2,25 @@ const express = require('express');
 const ensureAuthenticated = require('../helpers/auth');
 const router = express.Router();
 const Consultation = require('../models/Booking');
+const OrderItems = require('../models/OrderItems');
+const db = require('../config/DBConfig');
+const { QueryTypes } = require('sequelize');
 
 
-// ROUTING: 
+// ROUTING:
+router.get('/settings', ensureAuthenticated, async (req, res) => {
+    //  todo: query for roomURL after saving the room url to the db
+    let consult_detail = await db.query(`SELECT prod_name, date, start_time, end_time
+                                    FROM orderitems oi
+                                    INNER JOIN consultations c
+                                    ON oi.tutor_id = c.userId
+                                    WHERE cust_id = '${req.user.id}' and prodType = 'consultation session' 
+                                    GROUP BY oi.prod_name`, { type: QueryTypes.SELECT });
+
+    res.render('dashboard/student/consultationview', { consult_detail, layout: 'main2' });
+});
+
+
 // route to catalogue for consultation
 router.get('/listConsultations', ensureAuthenticated, (req, res) => {
     Consultation.findAll({
