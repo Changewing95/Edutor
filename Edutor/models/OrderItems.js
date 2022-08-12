d40d924b1229367583498887600056c302965f49
ruri flora newman <92986640+rurinewman@users.noreply.github.com>
2022-08-12 10:11:53 +0800

    update orderitems
    to standardise w yonglin

--- Edutor/models/OrderItems.js
+++ Edutor/models/OrderItems.js
@@ -1,3 +1,4 @@
+const sequelize = require('sequelize');
 const Sequelize = require('sequelize');
 const db = require('../config/DBConfig');
 
@@ -13,14 +14,17 @@ const OrderItems = db.define('orderItems',
         cust_name: { type: Sequelize.STRING },
         cust_id: { type: Sequelize.STRING },
         tutor_id: { type: Sequelize.STRING },
-        tutor_name: { type: Sequelize.STRING },
         prod_name: { type: Sequelize.STRING },
         prodType: { type: Sequelize.STRING },
         qty: { type: Sequelize.INTEGER },
         status: { type: Sequelize.STRING },
         price: { type: Sequelize.DECIMAL(8, 2) },
         item_detail: { type: Sequelize.STRING },
-        left_review: { type: Sequelize.STRING },
+        leftReview:
+        {
+            type: sequelize.BOOLEAN,
+            defaultValue: false,
+        }
         //foreign key is order id from Orders table (model)
     });
 
