ae02c7f6bfaec728023d0c67b61b8b7fd433af99
yonglinnnnnn <yongglinx@gmail.com>
2022-08-12 09:58:07 +0800

    push final thing

--- Edutor/routes/sentimentanalysis.js
+++ Edutor/routes/sentimentanalysis.js
@@ -15,17 +15,21 @@ router.post('/s-analyzer', function (req, res, next) {
 
     // data pre-process 
     const lexedReview = aposToLexForm(review);      // remove contractions
-    
+    console.log("lexed data: ", lexedReview);
+
     // set words to lower case
     const casedReview = lexedReview.toLowerCase();
+    console.log("lowercase: ", casedReview);
 
     // remove non-alphabetical and special characters
     const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
+    console.log("only alpha: ", alphaOnlyReview);
 
     // tokenization
     const { WordTokenizer } = natural;
     const tokenizer = new WordTokenizer();
     const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+    console.log("tokenized data: ", tokenizedReview);
 
     // correct misspelled words
     tokenizedReview.forEach((word, index) => {
@@ -34,10 +38,12 @@ router.post('/s-analyzer', function (req, res, next) {
 
     // removing stop words ('a', 'or', 'what')
     const filteredReview = SW.removeStopwords(tokenizedReview);
+    console.log("after removing stop words: ", filteredReview);
 
     const { SentimentAnalyzer, PorterStemmer } = natural;
     const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
     const analysis = new analyzer.getSentiment(filteredReview);
+    console.log("sentiment score: ", analysis);
 
     res.status(200).json({ analysis });
 });
