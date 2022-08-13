304482c0c933a315aee7f91c10622a49cf9b11dd
Aurzen205 <dylan205mong@gmail.com>
2022-08-13 22:45:46 +0800

    Additional info for event

--- Edutor/routes/studentEvent.js
+++ Edutor/routes/studentEvent.js
@@ -16,7 +16,14 @@ router.get('/main', (req, res) => {
         })
         .catch(err => console.log(err));
 });
+router.get('/display/:id', (req, res) => {
 
+    Event.findByPk(req.params.id)
+        .then((event) => {
+            res.render('student/detailedevent', { event });
+        })
+        .catch(err => console.log(err));
+});
 
 
 module.exports = router;
