cf4e5874808de3ac4d62325cc6ffff87e6c63a59
yonglinnnnnn <yongglinx@gmail.com>
2022-08-14 16:11:56 +0800

    fix validation for consultation (create and update)

--- Edutor/routes/main.js
+++ Edutor/routes/main.js
@@ -128,14 +128,14 @@ router.post('/vidroom/:id/', function (req, res) {
 	let link = req.body.roomURL;
 
 	// tried to save link with ip address -- cmi on the remote laptop
-	// let l = link.slice(16, 100);
-	// let ip_address = 'http://192.168.1.8';
-	// let url = ip_address.concat('', l);
-	// console.log(url);
-	// console.log('req.body.roomURL: ',req.body.roomURL);
+	let l = link.slice(16, 100);
+	let ip_address = 'http://192.168.1.8';
+	let url = ip_address.concat('', l);
+	console.log(url);
+	console.log('req.body.roomURL: ',req.body.roomURL);
 	
 	Consultation.update(
-		{ roomURL: link },
+		{ roomURL: url },
 		{ where: { id: `${req.params.id}` } }
 	)
 		.then((result) => {
--- Edutor/routes/review.js
+++ Edutor/routes/review.js
@@ -29,6 +29,11 @@ const natural = require('natural');
 const stopword = require('stopword');
 const spamCheck = require('spam-check');
 
+function calculatePercentage(ratingCount, totalCount) {
+    percentage = 0;
+    percentage = ratingCount / totalCount * 100;
+    return percentage;
+}
 
 // function to checkdate endtime > current:
 function checkEndTimeCurrentTime(array, type) {
@@ -236,22 +241,73 @@ router.get('/choose', ensureAuthenticated, async (req, res) => {
                                     FROM orderitems
                                     INNER JOIN users
                                     ON users.id = orderitems.tutor_id
-                                    WHERE orderitems.leftReview = false
+                                    WHERE orderitems.leftReview = false AND orderitems.cust_id = '${req.user.id}'
                                     `, { type: QueryTypes.SELECT });
-    // console.log(product)
+    console.log(product)
     res.render('review/choose', { orders: product });
 });
 
 // view tutor reviews for under product listings
-router.get('/:tutorid', ensureAuthenticated, (req, res) => {
+router.get('/:tutorid', ensureAuthenticated, async (req, res) => {
+    // Review.findAll({
+    //     where: { tutor_id: `${req.params.tutorid}` },
+    //     order: [['createdAt']],
+    //     raw: true
+    // })
+    //     .then((reviews) => {
+    //         // pass object to consultation.hbs
+    //         res.render('review/tutorReviews', { reviews });
+    //     })
+    //     .catch(err => console.log(err));
+    var userId = req.params.tutorid;
+    // query avg rating
+    let avgRating = await db.query(`SELECT AVG(rating) as 'avgRating'
+                                    FROM reviews
+                                    WHERE tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    // query for review count for each rating (/5)
+    let rating5Count = await db.query(`SELECT COUNT(rating) as 'rate5'
+                                    FROM reviews
+                                    WHERE rating = 5 and tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    let rating4Count = await db.query(`SELECT COUNT(rating) as 'rate4'
+                                    FROM reviews
+                                    WHERE rating = 4 and tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    let rating3Count = await db.query(`SELECT COUNT(rating) as 'rate3'
+                                    FROM reviews
+                                    WHERE rating = 3 and tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    let rating2Count = await db.query(`SELECT COUNT(rating) as 'rate2'
+                                    FROM reviews
+                                    WHERE rating = 2 and tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    let rating1Count = await db.query(`SELECT COUNT(rating) as 'rate1'
+                                    FROM reviews
+                                    WHERE rating = 1 and tutor_id = '${userId}'`, { type: QueryTypes.SELECT });
+    let totalCount = await db.query(`SELECT COUNT(*) AS 'totalrating' FROM reviews`, { type: QueryTypes.SELECT });
+
+    // for percentage count
+    const percentage5 = calculatePercentage(rating5Count[0].rate5, totalCount[0].totalrating);
+    const percentage4 = calculatePercentage(rating4Count[0].rate4, totalCount[0].totalrating);
+    const percentage3 = calculatePercentage(rating3Count[0].rate3, totalCount[0].totalrating);
+    const percentage2 = calculatePercentage(rating2Count[0].rate2, totalCount[0].totalrating);
+    const percentage1 = calculatePercentage(rating1Count[0].rate1, totalCount[0].totalrating);
+    const avgpercentage = calculatePercentage(avgRating[0].avgRating, 5)
+
+
+    // query for all reviews
     Review.findAll({
-        where: { tutor_id: `${req.params.tutorid}` },
-        order: [['createdAt']],
+        where: { tutor_id: req.params.tutorid },
+        attributes: [
+            'id', 'title', 'category', 'image', 'rating', 'description', 'username', 'sentiment_score', 'createdAt'],
+        order: [['rating', 'DESC'],['title', 'ASC']],
+        // where: { tutor_id: req.user.id },
         raw: true
     })
         .then((reviews) => {
-            // pass object to consultation.hbs
-            res.render('review/tutorReviews', { reviews });
+            // pass object to review.hbs
+            console.log(reviews);
+            res.render('review/tutorReviews.hbs', {
+                reviews, avgRating: avgRating[0], rating5: rating5Count[0], rating4: rating4Count[0],
+                rating3: rating3Count[0], rating2: rating2Count[0], rating1: rating1Count[0],
+                percentage5, percentage4, percentage3, percentage2, percentage1, avgpercentage
+            });
         })
         .catch(err => console.log(err));
 
@@ -395,7 +451,8 @@ router.post('/create/:prodType/:prodname', ensureAuthenticated, async (req, res)
                             {
                                 where: {
                                     cust_id: `${req.user.id}`,
-                                    prod_name: `${req.params.prodname}`
+                                    prod_name: `${req.params.prodname}`,
+                                    prodType: `${req.params.prodType}`
                                 }
                             })
                             .then((result) => {
